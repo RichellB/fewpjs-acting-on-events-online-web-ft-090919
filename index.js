3037d5efd7495ac18565049543b1f935a4c1c5ea
Richell Bonfield <richell.bonfield12@gmail.com>
2020-01-08 11:51:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 // Your code here
 dodger.style.backgroundColor = "#FF69B4";
-dodger.style.bottom = "0px";
 
 
-let dodger = document.getElementById("dodger");
-
+var dodger = document.getElementById("dodger");
 function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
- 
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left - 1}px`
   }
 }
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
