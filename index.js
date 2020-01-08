3ec1e2b5ed4d6114da603ef195e9ab209a2916ae
Richell Bonfield <richell.bonfield12@gmail.com>
2020-01-08 11:47:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "0px";
 
-document.addEventListener("keydown", function(e) {
-  console.log(e.key);
-});
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
